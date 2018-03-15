63cc872b217c2c2e78147e6a5efe63ac071dbe30
David Osuna <davidosuna@Davids-MacBook-Pro.local>
2018-03-15 01:56:38 +0100

    Added update company request

---
+++ app/Http/Requests/UpdateCompanyRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCompanyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->user();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'company' => 'required|string',
+            'title' => 'string|nullable',
+            'name' => 'required|string',
+            'surnames' => 'required|string',
+            'address_line_1' => 'required|string',
+            'address_line_2' => 'string|nullable',
+            'city' => 'required|string',
+            'country' => 'required|string',
+            'zip_code' => 'required|string',
+            'birthday' => 'date_format:Y-m-d|nullable'
+        ];
+    }
+}
