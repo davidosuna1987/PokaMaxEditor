58668fbc28e345b3a2ca73f54be7bfb2d55781a2
David Osuna <davidosuna1987@gmail.com>
2018-03-20 13:10:25 +0100

    Removed comentaries

--- app/Http/Controllers/PostcardController.php
+++ app/Http/Controllers/PostcardController.php
@@ -16,28 +16,17 @@ use App\Http\Requests\CreatePostcardRequest;
 
 class PostcardController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $postcards = Postcard::with('user', 'user.address', 'senderAddress', 'recieverAddresses', 'senderAddress.address', 'recieverAddresses.address')->get();
         return view('pages.postcards.index', compact('postcards'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(CreatePostcardRequest $request)
     {
         // return response()->json(['request' => $request->all()]);
@@ -149,39 +138,23 @@ class PostcardController extends Controller
             ]);
 
         endif;
-
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Postcard $postcard)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Postcard $postcard)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, $postcard_id)
     {
         return response()->json(['message' => "Your postcard was updated correctly!"]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Postcard  $postcard
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Postcard $postcard)
     {
         //
